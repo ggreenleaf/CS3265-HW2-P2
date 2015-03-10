3a000805ce0ab1897c32e665cae3e6ffd8aff733
Geoffrey <geoffrey.greenleaf@gmail.com>
2015-03-10 11:49:16 -0500

    added psuedocode for backtacking alg

--- cspsolver.py
+++ cspsolver.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-
+from sys import argv
 def parse_vars(filename):
 
 	variables = OrderedDict()
@@ -23,16 +23,18 @@ def parse_cons(filename):
 
 
 class cspsolver:
-	def __init__(self,var_fname, con_fname):
+	def __init__(self,var_fname, con_fname, fc):
 		self.variables = parse_vars(var_fname) #dictionary of lists key is var and val is domain list 
 		self.cons = parse_cons(con_fname) #string of equations used to evaluate 
-			
+		self.fc = fc
+
 	def display_assignments(self):
 		for key in self.variables.keys():
 			assignment_index = self.variables[key][-1]
 			print key, self.variables[key][0][assignment_index]
 	
 	def eval_constraints(self):
+		'''evaluate the constraints in self.cons if one is false return false'''
 		for con in self.cons:
 			key1 = con[0] #all variables are singal
 			op = con[1]
@@ -40,8 +42,19 @@ class cspsolver:
 			
 			var1 = self.variables[key1][0][self.variables[key1][-1]]
 			var2 = self.variables[key2][0][self.variables[key2][-1]]
-			if not eval(var1+op+var2):
-				return False #constraints failed
+			if not eval("%s%s%s"%var1,op,var2): #only return false if one is false
+				return False
 
+	def backtracking (self, assignment, csp):
+		# if assignment is complete then return assignment 
+		# var = Select-Unassigned-Variable(Variable[csp], assignment, csp)
+		# for each value in Order-Domain_values(var, assignment, csp)
+		#	if value is consistent with assignment according to Constraints
+		#		add var=value to assignment
+		#   	result = Backtracking(assignment, csp)
+		#    	if result != failure then return result
+		#		remove {var = value} from assignment
+		# return failure
 if __name__ == "__main__":
-	solver = cspsolver("ex2.var","ex2.con")
+	script, fvar,fcon,fc = argv
+	solver = cspsolver(fvar,fcon,fc)
